f5403957b59f2e40d96abc094c3a62f3143ee160
hrqlp <henriqueodevelp@gmail.com>
2021-07-02 04:43:32 -0300

    resolução elaborada do desafio_01

--- desafio_01.py
+++ desafio_01.py
@@ -1,17 +1,18 @@
-print("#############################################################")
-print("##  Bonificação de Final de ano Estacionamento Seu Jertuz  ##")
-print("#############################################################")
+#vinheta definida em uma variavel de string com quebras de linhas
+vinheta = "#############################################################\n##  Bonificação de Final de ano Estacionamento Seu Jertuz  ##\n#############################################################"
+print(vinheta)
 
 funcao = int(input("Digite o numero da sua função: \n 1 - Segurança; \n 2 - Caixa; \n 3 - Manobrista; \nn: "))
 salario = float(input("Digite o valor de seu salário: "))
-
+# criando variavel padrão para o resultado 
+msg_bonus = ".\nO seu salário com bonus será: R$"
 if(funcao == 1):
-    salario = salario * 1.09
-    print("O seu salário com bonus será: R$",salario)
+    # veja que podemos fazer o calculo do bonus diretamente no print 
+    # e alem disso tambem podemos usar a função round(valor, casas_decimais) que fará o arredondamento do resultado do calculo e definira quantas casas decimais será exibida.
+    print(msg_bonus,round(salario * 1.09, 2))
 elif(funcao == 2):
-    print("O seu salário com bonus será: R$",salario)
+    print(msg_bonus,round(salario * 1.10, 2))
 elif(funcao == 3):
-    salario = salario * 1.12
-    print("O seu salário com bonus será: R$", salario)
+    print(msg_bonus, round(salario * 1.12, 2))
 else:
     print("\nOps, você não digitou um numero de função valido.")
